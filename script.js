27f928f75a92be689dbfc7281123934e51ed8b28
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2020-06-20 16:43:22 -1000

    Celcius temp add for the rest of the world.

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 var APIKey = "4af87ee91531ff09b1ce9e3392587b3a"
-var weatherUrl ="https://api.openweathermap.org/data/2.5/weather?q=seattle&appid="+APIKey
+var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=seattle&appid=" + APIKey
+
+
 
 $.ajax({
     url: weatherUrl,
-    method:"GET"
-}) 
-  .then(function(response){
-       // Log the queryURL
-      console.log(weatherUrl)
-    // Log the resulting object
-    console.log(response) 
+    method: "GET"
+})
+    .then(function (response) {
+        // Log the queryURL
+        console.log(weatherUrl)
+        // Log the resulting object
+        console.log(response)
 
-      $(".city").html("<h2>"+response.name + " Weather Details</h2>")
-      $(".temp").text("Temp: "+response.main.temp)
-      $(".humidity").text("Humidity: "+response.main.humidity)
-      $(".wind").text("Wind: " + response.wind.speed)
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        var tempC = (response.main.temp - 273.15);
 
-      
-      
-  })
+        $(".city").html("<h2>" + response.name + " Weather Details</h2>");
+        $(".temp").text("Temp (F): " + tempF.toFixed());
+        $(".tempC").html("Temp (C): "+ tempC.toFixed())
+        $(".humidity").text("Humidity: " + response.main.humidity);
+        $(".wind").text("Wind: " + response.wind.speed);
+
+        
+
+
+
+
+
+    })
