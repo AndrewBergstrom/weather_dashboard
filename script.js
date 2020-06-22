da2e82c135857e27dfa72ca02cc4739e22a42da5
Andrew B <65259490+AndrewBergstrom@users.noreply.github.com>
2020-06-21 20:10:40 -1000

    city search history now saving

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var APIKey = "4af87ee91531ff09b1ce9e3392587b3a"
 var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=seattle&appid=" + APIKey
 
+var fiveDayQuery = "https://api.openweathermap.org/data/2.5/forecast?q=seattle&appid=" + APIKey
+
+var startDate = moment().format('M/DD/YYYY');  // Current Date
+
 // 5 day forcast variables
 var dayOne = $("#dayOne")
 var dayTwo = $("#dayTwo")
@@ -29,7 +33,7 @@ $.ajax({
         $(".tempC").html("Temp (C): " + tempC.toFixed())
         $(".humidity").text("Humidity: " + response.main.humidity);
         $(".wind").text("Wind: " + response.wind.speed);
-       
+
         // using iconImg and dynamically creating and image element, then appending it to the weather-icon classs
         var iconImg = $("<img id = 'icon'>")
         $(".weather-icon").append(iconImg)
@@ -40,9 +44,9 @@ $.ajax({
         // taking the correct weather icon from our link, turning it into a .png and taking the dynamically created icon ID, and say the attribute we would like to add is the source, and that source is using the iconUrl variable to tell our system where to look.
         var iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
         console.log(iconUrl, icon)
-            $('#icon').attr('src', iconUrl);
+        $('#icon').attr('src', iconUrl);
 
-            // This is grabbing the lattitude and longitude
+        // This is grabbing the lattitude and longitude
         var myLat = response.coord.lat;
         var myLon = response.coord.lon;
         getUVindex(myLat, myLon);
@@ -55,7 +59,7 @@ $.ajax({
                 url: `https://api.openweathermap.org/data/2.5/uvi?appid=${APIKey}&lat=${lat}&lon=${log}`,
                 method: "GET",
                 success: function (data) {
-                     console.log("data from uv index: ", data);
+                    console.log("data from uv index: ", data);
                     $(".uvIndex").text(`UV index: ${data.value}`);
                 }
             })
@@ -64,6 +68,29 @@ $.ajax({
     })
 
 
+$.ajax({
+    url: fiveDayQuery,
+    method: "GET"
+
+}).then(function (fiveDayRes) {
+    // log 5 day query
+    console.log(fiveDayQuery)
+
+    console.log("five day", fiveDayRes.list)
+
+    var fiveDayOnly = []
+
+    for (var i = 0; i < fiveDayRes.list.length; i++) {
+
+        if (fiveDayRes.list[i].dt_txt.indexOf("12" !== -1)) {
+
+            console.log(fiveDayRes.list[i].dt_txt.indexOf("12"))
+
+            fiveDayOnly.push(fiveDayRes.list[i])
+
+        }
+    }
+})
 
 
 
@@ -73,14 +100,17 @@ $.ajax({
 
 
 
+// On click event listener for search button
+$("#citySearch").on("click", function () {
+    var city = $("#cityInput").val();
+    var cityHistory = [];
 
+    cityHistory = JSON.parse(localStorage.getItem("cityHistory")) || []; 
+    cityHistory.push("cityHistory", JSON.stringify(cityHistory));
 
-//On click event listener for search button
-// $("#run-search").on("click", function () {
-//     city = $("#search-term").val()
-//     displayWeather()
-//     display5day()
-// })
+    displayWeather()
+    display5day()
+})
 
 //On click event listener for city buttons
 // $(document).on("click", ".city-btn", function () {
